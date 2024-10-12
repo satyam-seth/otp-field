5fefaef02de8c6c50cf2b7721819f8bad0d7c4b4
Satyam Seth <satyam1998.1998@gmail.com>
2024-10-12 21:04:07 +0530

    add support for custom regex

--- src/ts/components/types.ts
+++ src/ts/components/types.ts
@@ -1,7 +1,36 @@
+/* eslint-disable no-unused-vars */
+// eslint-disable-next-line no-shadow
+export enum OTPValueType {
+  // Match anything except digits
+  NUMERIC,
+
+  // Match anything except alphabetic characters
+  ALPHABETIC,
+
+  // Match anything except alphabetic lower characters
+  ALPHABETIC_LOWER,
+
+  // Match anything except alphabetic upper characters
+  ALPHABETIC_UPPER,
+
+  // Match anything except alphanumeric characters
+  ALPHANUMERIC,
+
+  // Match anything except alphanumeric lower characters
+  ALPHANUMERIC_LOWER,
+
+  // Match anything except alphanumeric upper characters
+  ALPHANUMERIC_UPPER,
+}
+
 export interface OTPFieldConfig {
   namespace: string;
 
   boxCount: number;
 
   onPasteBlur: boolean;
+
+  valueType?: OTPValueType;
+
+  customRegex?: RegExp;
 }
