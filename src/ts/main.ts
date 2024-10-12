37e0dc5a386793b6766ab93d6f964298f4bf704d
Satyam Seth <satyam1998.1998@gmail.com>
2024-10-13 01:34:59 +0530

    add doc string and remove log messages

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -7,7 +7,7 @@ window.onload = () => {
   // create otp field instance
   const otpField = new OTPField({
     namespace: 'example',
-    boxCount: 6,
+    boxCount: 3,
   });
 
   // TODO: add for to get config and add button to show current value
