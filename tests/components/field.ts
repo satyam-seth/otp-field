24eff7d2a032154120d6c9cfdc0d01cd1cec8368
Satyam Seth <satyam1998.1998@gmail.com>
2025-04-06 21:26:46 +0530

    field skeleton should return correct html element

--- tests/components/field.ts
+++ tests/components/field.ts
@@ -1,7 +1,11 @@
+/* eslint-disable no-unused-expressions */
 import { expect } from 'chai';
-import { describe, it } from 'mocha';
+import { afterEach, beforeEach, describe, it } from 'mocha';
+import sinon from 'sinon';
 import { OTPField } from '../../src/ts/components/field';
 import { OTPFieldConfig } from '../../src/ts/components/types';
+
+const jsdom = require('jsdom-global');
 
 describe('Test OTPField component', () => {
   const validConfig: OTPFieldConfig = {
@@ -13,6 +17,20 @@ describe('Test OTPField component', () => {
     namespace: 'test-namespace',
     boxCount: 0,
   };
+
+  let cleanup: any;
+
+  beforeEach(() => {
+    cleanup = jsdom();
+  });
+
+  afterEach(() => {
+    // cleanup jsdom
+    cleanup();
+
+    // Restore the spies
+    sinon.restore();
+  });
 
   it('should not throw an error for valid config', () => {
     expect(() => new OTPField(validConfig)).to.not.throw();
@@ -31,5 +49,38 @@ describe('Test OTPField component', () => {
 
     // Assert that id is correct
     expect(field.id).to.equal('otp-field-test-namespace');
+  });
+
+  it('skeleton should return correct html element', () => {
+    // Create OTPField instance
+    const field = new OTPField(validConfig);
+
+    // Create spy for getBoxElement method
+    // @ts-ignore
+    const getBoxElementSpy = sinon.spy(field, 'getBoxElement');
+
+    // Access skeleton
+    // eslint-disable-next-line
+    const skeleton = field['skeleton'](); // bypass private;
+
+    // Assert that the skeleton is an HTMLElement
+    expect(skeleton).to.be.instanceOf(HTMLElement);
+
+    // Assert that the skeleton has the correct tag name
+    expect(skeleton.tagName).to.be.equal('DIV');
+
+    // Assert that the skeleton has the correct class name
+    expect(skeleton.className).to.be.equal('otp-field');
+
+    // Assert that the spy was called exactly 4 times with specific arguments
+    expect(getBoxElementSpy.callCount).to.be.equal(validConfig.boxCount);
+
+    for (let i = 0; i < validConfig.boxCount; i += 1) {
+      expect(getBoxElementSpy.getCall(i).args[0]).to.be.equal(i);
+
+      // Assert that titleElement is appended in skeleton
+      expect(skeleton.querySelector(`#${validConfig.namespace}-box-${i}`)).to
+        .exist;
+    }
   });
 });
